23313e98632f406b633a03dcf78687ee091adee2
Ryan Yogan <ryanyogan@gmail.com>
2024-01-29 17:52:10 -0600

    feat // beinmg silly

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,27 +11,34 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="h-full flex flex-col items-center pt-20 px-10 bg-slate-900">
+      <img src="/remix-logo-new@dark.png" width="402" height="149" />
       <div className="space-y-4 max-w-md text-lg text-slate-300">
-        <p className="text-gray-100 text-xl">
-          Do you have bad fucking ADHD and hate all project management software
-          overtime?
+        <p>
+          This is a demo app to compare Remix to Next.js for a simple
+          application, it seems to be pretty damn fast ;)
         </p>
+        <p>
+          It's a recreation of the popular drag and drop interface in{" "}
+          <a href="https://trello.com" className="underline">
+            Trello
+          </a>{" "}
+          and other similar apps.
+        </p>
+        <p>If you want to play around, click sign up!</p>
       </div>
       <div className="flex w-full justify-evenly max-w-md mt-8 rounded-3xl p-10 bg-slate-800">
         <Link
           to="/signup"
           className="text-xl font-medium text-brand-aqua underline"
         >
-          Sign Up
+          Sign up
         </Link>
-
-        <div className="h-full border-r border-l-sky-500" />
-
+        <div className="h-full border-r border-slate-500" />
         <Link
           to="/login"
           className="text-xl font-medium text-brand-aqua underline"
         >
-          Log In
+          Login
         </Link>
       </div>
     </div>
