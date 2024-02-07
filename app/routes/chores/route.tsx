b65f662203cf57b155bd5d5f40746347974914c2
Ryan Yogan <ryanyogan@gmail.com>
2024-02-06 21:50:38 -0600

    wip

--- app/routes/chores/route.tsx
+++ app/routes/chores/route.tsx
@@ -1,25 +1,89 @@
-import { Payment, columns } from "./columns";
-import { DataTable } from "./data-table";
+import { Link, NavLink } from "@remix-run/react";
+import { PlusIcon } from "lucide-react";
+import { NewBoard } from "~/components/new-board";
 
-export default function ChoreRoute() {
-  const data: Payment[] = [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "489e1d42",
-      amount: 125,
-      status: "processing",
-      email: "example@gmail.com",
-    },
-  ];
+export const meta = () => {
+  return [{ title: "Boards" }];
+};
 
+export default function Projects() {
   return (
-    <div>
-      <DataTable columns={columns} data={data} />
+    <div className="h-full flex flex-col">
+      <div className="flex flex-row bg-slate-800/30 shadow-md p-0 justify-between border-b border-slate-800">
+        <div className="ml-4 flex flex-row items-center">
+          <NavLink
+            to="/home"
+            prefetch="intent"
+            className={({ isActive }) =>
+              `text-sm font-medium underline-offset-2 text-left text-green-400 px-2 py-1 ${isActive && "underline"}`
+            }
+          >
+            Boards
+          </NavLink>
+          <NavLink
+            to="/chores"
+            prefetch="intent"
+            className={({ isActive }) =>
+              `text-sm font-medium underline-offset-2 text-left text-green-400 px-2 py-1 ${isActive && "underline"}`
+            }
+          >
+            Chores
+          </NavLink>
+          <NavLink
+            to="/activity"
+            prefetch="intent"
+            className={({ isActive }) =>
+              `text-sm font-medium underline-offset-2 text-left text-green-400 px-2 py-1 ${isActive && "underline"}`
+            }
+          >
+            Activity
+          </NavLink>
+          <NavLink
+            to="/settings"
+            prefetch="intent"
+            className={({ isActive }) =>
+              `text-sm font-medium underline-offset-2 text-left text-green-400 px-2 py-1 ${isActive && "underline"}`
+            }
+          >
+            Settings
+          </NavLink>
+        </div>
+        <NewBoard />
+      </div>
+      <Boards />
     </div>
+  );
+}
+
+function Boards() {
+  return (
+    <div className="p-6">
+      <nav className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        <NewChore />
+        {/* {boards.map((board) => (
+            <Board
+              key={board.id}
+              name={board.name}
+              id={board.id}
+              color={board.color}
+              itemCount={board.items.length}
+              shareable={board.shareable}
+            />
+          ))} */}
+      </nav>
+    </div>
+  );
+}
+
+function NewChore({}: {}) {
+  return (
+    <Link
+      to={`/board/`}
+      className="flex w-full h-28 sm:h-40 p-4 justify-center items-center rounded-sm border-l-2 border-slate-700/50 border shadow hover:shadow-xl bg-slate-800/50 relative hover:bg-slate-800/80"
+    >
+      <div className="font-semibold text-ellipsis text-blue-400">
+        <PlusIcon className="h-20 w-20 text-slate-700" />
+      </div>
+    </Link>
   );
 }
