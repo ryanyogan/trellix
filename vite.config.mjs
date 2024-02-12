50f2302b8a5280dc78ad517899c3be7d2153d193
Ryan Yogan <ryanyogan@gmail.com>
2024-02-12 15:45:02 -0600

    dev // temp removal of monitoring

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,17 +1,16 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), remix({
-    ignoredRouteFiles: ["**/.*"],
-  }), sentryVitePlugin({
-    org: "ryan-yogan-81ab02b9",
-    project: "trellix-clone"
-  })],
+  plugins: [
+    tsconfigPaths(),
+    remix({
+      ignoredRouteFiles: ["**/.*"],
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
 });
