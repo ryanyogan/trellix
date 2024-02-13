f25aa302a65b674a6e73a611c6b949baef232127
Ryan Yogan <ryanyogan@gmail.com>
2024-02-12 20:25:42 -0600

    multi-region config

--- start.js
+++ start.js
@@ -1,7 +1,22 @@
 import { spawn } from "child_process";
+import { getInstanceInfo } from "litefs-js";
 
 async function start() {
   await execute("npx prisma migrate deploy");
+
+  const { currentInstance, currentIsPrimary, primaryInstance } =
+    await getInstanceInfo();
+
+  if (currentIsPrimary) {
+    console.log(
+      `Instance (${currentInstance}) in ${process.env.FLY_REGION} is primary. Deploying migrations.`,
+    );
+    await exec("npx prisma migrate deploy");
+  } else {
+    console.log(
+      `Instance (${currentInstance}) in ${process.env.FLY_REGION} is not primary (the primary instance is ${primaryInstance}). Skipping migrations.`,
+    );
+  }
 
   console.log("[APP] - Starting Application in production mode...");
   await execute("NODE_ENV=production remix-serve ./build/server/index.js");
